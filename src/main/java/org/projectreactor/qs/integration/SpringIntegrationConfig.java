a36e176dd2a79f30be98b5835044ebc6f4c19107
Jon Brisbin <jon@jbrisbin.com>
2014-02-14 15:00:30 -0600

    Change config to use byte[] codec.

--- src/main/java/org/projectreactor/qs/integration/SpringIntegrationConfig.java
+++ src/main/java/org/projectreactor/qs/integration/SpringIntegrationConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 import org.springframework.integration.config.ConsumerEndpointFactoryBean;
 import org.springframework.integration.ip.tcp.TcpReceivingChannelAdapter;
@@ -20,8 +21,10 @@ import org.springframework.messaging.MessagingException;
 import org.springframework.util.StopWatch;
 import reactor.core.Environment;
 import reactor.io.Buffer;
+import reactor.io.encoding.Codec;
 import reactor.io.encoding.LengthFieldCodec;
 import reactor.io.encoding.PassThroughCodec;
+import reactor.io.encoding.StandardCodecs;
 import reactor.tcp.config.ServerSocketOptions;
 
 import java.util.Map;
@@ -33,6 +36,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Jon Brisbin
  */
 @Configuration
+@ImportResource("org/projectreactor/qs/integration/common.xml")
 public class SpringIntegrationConfig {
 
 	@Value("${reactor.port:3000}")
@@ -81,23 +85,29 @@ public class SpringIntegrationConfig {
 	@SuppressWarnings({"unchecked", "rawtypes"})
 	@Bean
 	@Profile("reactor")
-	public ReactorTcpInboundChannelAdapter reactorTcpChannelAdapter(Environment env, MessageChannel output) {
+	public ReactorTcpInboundChannelAdapter reactorTcpChannelAdapter(Environment env,
+	                                                                MessageChannel output) {
 		ReactorTcpInboundChannelAdapter tcp = new ReactorTcpInboundChannelAdapter(env,
 		                                                                          tcpPort,
 		                                                                          dispatcher);
+
+		Codec delegateCodec = StandardCodecs.BYTE_ARRAY_CODEC;
+//		Codec delegateCodec = new PassThroughCodec<Buffer>() {
+//			@Override
+//			protected Buffer beforeAccept(Buffer b) {
+//				// pretend like we did something with the data
+//				return b.skip(b.remaining());
+//			}
+//		};
+		Codec codec = new LengthFieldCodec(delegateCodec);
+
 		return tcp.setOutput(output)
 		          .setServerSocketOptions(new ServerSocketOptions()
 				                                  .tcpNoDelay(true)
 				                                  .backlog(1000)
 				                                  .rcvbuf(1048576)
 				                                  .sndbuf(1048576))
-		          .setCodec(new LengthFieldCodec(new PassThroughCodec<Buffer>() {
-			          @Override
-			          protected Buffer beforeAccept(Buffer b) {
-				          // pretend like we did something with the data
-				          return b.skip(b.remaining());
-			          }
-		          }));
+		          .setCodec(codec);
 	}
 
 	@Bean
@@ -131,18 +141,22 @@ public class SpringIntegrationConfig {
 
 			private final Map<Object, StopWatch> stopWatches = new ConcurrentHashMap<>();
 
+			private final Map<Object, Integer> startCounts = new ConcurrentHashMap<>();
+
 			@Override
 			public void onApplicationEvent(TcpConnectionEvent event) {
 				if(event instanceof TcpConnectionOpenEvent) {
 					StopWatch stopWatch = new StopWatch();
 					stopWatches.put(event.getSource(), stopWatch);
 					stopWatch.start();
+					startCounts.put(event.getSource(), msgCnt.getCount());
 				} else if(event instanceof TcpConnectionCloseEvent) {
 					stopWatches.get(event.getSource()).stop();
+					int endCount = (msgCnt.getCount() - startCounts.remove(event.getSource()));
 					long time = stopWatches.get(event.getSource()).getLastTaskTimeMillis();
 					// TODO keep count per socket, currently only works with 1
 					log.info("throughput this session: {}/sec in {}ms",
-					         (int)((msgCnt.getCount() * 1000) / time), time);
+					         (int)((endCount * 1000) / time), time);
 				}
 			}
 		};
